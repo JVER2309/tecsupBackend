d8239ad534a05dda75e97204eeddd88758291b80
JVER2309 <jretuertop@uni.pe>
2022-08-06 10:07:43 -0500

    semana08 dia06

--- dia05/index.js
+++ dia05/index.js
@@ -1,12 +1,14 @@
-const { config } = require('./config');
 const express = require('express');
+const {config} = require('./config');
+const cors = require('cors');
 
-const MysqlLib = require('./lib/mysql');
-
-sql = new MysqlLib();
-
+const alumnoApi = require('./routes/alumno.route');
+const cursoApi = require('./routes/curso.route');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.json({
@@ -15,4 +17,6 @@ app.get('/',(req,res)=>{
     })
 })
 
+alumnoApi(app);
+cursoApi(app);
 app.listen(config.port,()=>console.log('servidor en http://localhost:'+config.port))
